87058618b7f28a6c1f68cf37818544b70d0f7a07
Kris <kristian.claire.101@gmail.com>
2019-10-15 08:20:54 -0700

    adds functionality to all movie cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
-import ReactDOM from "react-dom";
 import MovieList from './Movies/MovieList';
 import SavedList from './Movies/SavedList';
 import Movie from './Movies/Movie';
-import MovieCard from './Movies/MovieCard';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 
 const App = () => {
@@ -16,9 +14,9 @@ const App = () => {
 
   return (
     <div>
-      <SavedList list = {savedList} />
-      <Route exact path = "/" Component = {MovieList} />
-      <Route path = "/Movie/:id" Component = {Movie} exact />
+      <SavedList list={savedList} />
+      <Route exact path="/" component = {MovieList} />
+      <Route path = "/Movie/:id" component = {Movie} />
     </div>
   );
 };
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -13,7 +14,7 @@ const MovieList = props => {
         .catch(error => {
           console.error('Server Error', error);
         });
-    }
+    };
     
     getMovies();
   }, []);
@@ -21,11 +22,15 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
+        <div key={movie.id}>
+        <Link to = {`/Movies/${movie.id}`}>
         <MovieDetails key={movie.id} movie={movie} />
+        </Link>
+        </div>
       ))}
     </div>
   );
-}
+};
 
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
@@ -47,6 +52,6 @@ function MovieDetails({ movie }) {
       ))}
     </div>
   );
-}
+};
 
 export default MovieList;
